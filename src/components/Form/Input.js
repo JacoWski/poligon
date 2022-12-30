bbc1ce95a5e5059f5ba4ce501b85415ada673fef
JacoWski <69387137+JacoWski@users.noreply.github.com>
2022-12-30 20:16:27 +0100

    Ścieżki absolutne.

--- src/components/Form/Input.js
+++ src/components/Form/Input.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from 'prop-types';
 
-import { Colors } from "../../common";
+import { Colors } from "common";
 
 const Input = ({ bgColor, color, borderSize, borderRadius, borderColor }) => {
     const styles = {
