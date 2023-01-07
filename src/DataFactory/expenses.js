ca0ab305907fae6c0a0497849221ef1c0cd88725
JacoWski <69387137+JacoWski@users.noreply.github.com>
2023-01-07 14:56:35 +0100

    Expenses finished.

--- src/DataFactory/expenses.js
+++ src/DataFactory/expenses.js
@@ -1,10 +1,20 @@
-const expensesData = [
-    { id: 1, expense: "wydatek", name: "Test 1", amount: 100, category: "jedzenie" },
-    { id: 1, expense: "wydatek", name: "Test 2", amount: 100, category: "jedzenie" },
-    { id: 1, expense: "wydatek", name: "Test 3", amount: 100, category: "jedzenie" },
-    { id: 1, expense: "wydatek", name: "Test 4", amount: 100, category: "jedzenie" },
-    { id: 1, expense: "wydatek", name: "Test 5", amount: 100, category: "jedzenie" },
-    { id: 1, expense: "wydatek", name: "Test 6", amount: 100, category: "jedzenie" },
+const outcomeData = [
+    { id: 1, expense: "wydatek", name: "Test wydatek 1", amount: 100, category: "jedzenie" },
+    { id: 2, expense: "wydatek", name: "Test wydatek 2", amount: 100, category: "jedzenie" },
+    { id: 3, expense: "wydatek", name: "Test wydatek 3", amount: 100, category: "jedzenie" },
+    { id: 4, expense: "wydatek", name: "Test wydatek 4", amount: 100, category: "jedzenie" },
+    { id: 5, expense: "wydatek", name: "Test wydatek 5", amount: 100, category: "jedzenie" },
+    { id: 6, expense: "wydatek", name: "Test wydatek 6", amount: 100, category: "jedzenie" },
 ];
 
-export default expensesData;
+const incomeData = [
+    { id: 1, expense: "przychód", name: "Test przychód 1", amount: 100, category: "jedzenie" },
+    { id: 2, expense: "przychód", name: "Test przychód 2", amount: 100, category: "jedzenie" },
+    { id: 3, expense: "przychód", name: "Test przychód 3", amount: 100, category: "jedzenie" },
+    { id: 4, expense: "przychód", name: "Test przychód 4", amount: 100, category: "jedzenie" },
+    { id: 5, expense: "przychód", name: "Test przychód 5", amount: 100, category: "jedzenie" },
+    { id: 6, expense: "przychód", name: "Test przychód 6", amount: 100, category: "jedzenie" },
+];
+
+export default outcomeData;
+export {incomeData};
